98baf32b61df6ff8673aa6d0fba470d53944ad15
DevDilip28 <dilipasdeo028@gmail.com>
2026-04-21 17:12:15 +0530

    user_id

--- server/src/task/controller.py
+++ server/src/task/controller.py
@@ -2,57 +2,61 @@ from src.task.dtos import TaskSchema
 from sqlalchemy.orm import Session
 from src.task.models import TaskModel
 from fastapi import HTTPException
+from src.user.models import UserModel
 
-def create_task(body: TaskSchema, db: Session):
+def create_task(body: TaskSchema, db: Session, user: UserModel):
     data = body.model_dump()
 
-    new_task = TaskModel(title = data["title"],
-                         description = data["description"],
-                         is_completed = data["is_completed"]
-                         )
-    
+    new_task = TaskModel(
+        title=data["title"],
+        description=data["description"],
+        is_completed=data["is_completed"],
+        user_id=user.id
+    )
+
     db.add(new_task)
     db.commit()
     db.refresh(new_task)
 
     return new_task
 
-def get_tasks(db: Session):
-    tasks = db.query(TaskModel).all()
+def get_tasks(db: Session, user: UserModel):
+    tasks = db.query(TaskModel).filter(TaskModel.user_id == user.id).all()
 
     return tasks
 
-def get_task_by_id(task_id: int, db: Session):
-    task = db.query(TaskModel).get(task_id)
+def get_task_by_id(task_id: int, db: Session, user: UserModel):
+    task = db.query(TaskModel).filter(TaskModel.id == task_id,TaskModel.user_id == user.id).first()
 
-    if(not task):
+    if not task:
         raise HTTPException(status_code=404, detail="Task not found")
 
     return task
- 
-def update_task(task_id: int, body: TaskSchema, db: Session):
+
+
+def update_task(task_id: int, body: TaskSchema, db: Session, user: UserModel):
     data = body.model_dump()
 
-    task = db.query(TaskModel).get(task_id)
+    task = db.query(TaskModel).filter(TaskModel.id == task_id, TaskModel.user_id == user.id).first()
 
-    if(not task):
+    if not task:
         raise HTTPException(status_code=404, detail="Task not found")
-    
+
     for field, value in data.items():
         setattr(task, field, value)
 
-    db.add(task)
     db.commit()
     db.refresh(task)
 
     return task
 
-def delete_task(task_id: int, db: Session):
-    task = db.query(TaskModel).get(task_id)
 
-    if(not task):
+def delete_task(task_id: int, db: Session, user: UserModel):
+    task = db.query(TaskModel).filter(TaskModel.id == task_id, TaskModel.user_id == user.id).first()
+
+    if not task:
         raise HTTPException(status_code=404, detail="Task not found")
-    
+
     db.delete(task)
     db.commit()
 
--- server/src/task/router.py
+++ server/src/task/router.py
@@ -11,20 +11,20 @@ task_routes = APIRouter(prefix="/task")
 
 @task_routes.post("/create", response_model=TaskResponseSchema, status_code = status.HTTP_201_CREATED)
 def create_task(body: TaskSchema, db: Session = Depends(get_db), user: UserModel = Depends(is_authenticated)):
-    return controller.create_task(body, db)
+    return controller.create_task(body, db, user)
 
 @task_routes.get("/all_tasks", response_model=List[TaskResponseSchema], status_code = status.HTTP_200_OK)
 def get_tasks(db: Session = Depends(get_db), user: UserModel = Depends(is_authenticated)):
-    return controller.get_tasks(db)
+    return controller.get_tasks(db, user)
 
 @task_routes.get("/id_{task_id}", response_model=TaskResponseSchema, status_code = status.HTTP_200_OK)
 def get_task_by_id(task_id: int, db: Session = Depends(get_db), user: UserModel = Depends(is_authenticated)):
-    return controller.get_task_by_id(task_id, db)
+    return controller.get_task_by_id(task_id, db, user)
 
 @task_routes.put("/update/id_{task_id}", response_model=TaskResponseSchema, status_code = status.HTTP_200_OK)
 def update_task(body: TaskSchema, task_id: int, db: Session = Depends(get_db), user: UserModel = Depends(is_authenticated)):
-    return controller.update_task(task_id, body, db)
+    return controller.update_task(task_id, body, db, user)
 
 @task_routes.delete("/delete/id_{task_id}", response_model=None, status_code = status.HTTP_204_NO_CONTENT)
 def delete_task(task_id: int, db: Session = Depends(get_db), user: UserModel = Depends(is_authenticated)):
-    return controller.delete_task(task_id, db)
+    return controller.delete_task(task_id, db, user)
